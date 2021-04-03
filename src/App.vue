8daaf40ce17fab12fa1a6bc827b85987764ea014
Employee #427 <chcolatemousse@gmail.com>
2021-04-03 22:38:16 +0000

    added the styles_app.css file for App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,12 @@
     <div class="top">
       <div class="container-center">
         <div id="clockbox">
-          <script src="clock.js"></script>
+          <!--script src="clock.js"></script-->
         </div>
         <h1>Header here</h1>
       </div>
       <p id="github_code">
-        Code <a href="https://github.com/pisar-relva/telemetriaVue">here</a>
+        Code <a href="https://github.com/pisar-relva/telemetriaVue" target="_blank" rel="noopener noreferrer">here</a>
       </p>
     </div>
     <Main />
@@ -27,52 +27,5 @@ export default {
 </script>
 
 <style>
-/* For the whole app */
-* {
-  box-sizing: border-box;
-  margin: 0;
-  font-family:Klavika Basic;
-}
-#app {
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
-
-/* header */
-.top {
-  background-color: #009de0;
-  box-shadow:0px 5px 5px #D3D3D3;	
-  font-size: 15px;
-  display: flex;
-}
-
-/* "body" */
-.container-center {
-  display: flex;
-  height: auto;
-  width: auto;
-  margin: auto;
-}
-
-/* h1 in header */
-.top h1 {
-    margin: 5px;
-    color: white;
-    font-size: 2.5em;
-}
-
-/* for the letters */
-#github_code {
-  float: right;
-  color: white;
-  /*font-weight: bold;*/
-  font-size: 13px;
-  margin: 5px;
-}
-
- /* for the link */
-#github_code a {
-  color: white;
-}
-
+  @import './assets/styles_app.css';
 </style>
