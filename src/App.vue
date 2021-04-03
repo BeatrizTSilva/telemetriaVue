98b72dd9786d01e7e439aa64146a73ec78ac6d5a
Employee #427 <chcolatemousse@gmail.com>
2021-04-03 23:32:21 +0000

    clock.js is still not working

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
       <div class="container-center">
         <div id="clockbox">
           <!--script src="./assets/clock.js"></script-->
+          <!-- not linked to the file of the clock. it should not be here because this is a <template> -->
         </div>
         <h1>Header here</h1>
       </div>
@@ -17,8 +18,8 @@
 
 <script>
 import Main from './components/Main.vue'
-import clock_func from './assets/clock.js'
-
+//import clock_func from './assets/clock.js' /* not working */
+//clock_func();
 
 export default {
   name: 'App',
