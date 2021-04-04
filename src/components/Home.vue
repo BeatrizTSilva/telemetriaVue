2bf2c87a27203b70c6037f08253edee17e265eab
Employee #427 <chcolatemousse@gmail.com>
2021-04-04 22:15:25 +0000

    added Home.vue that shows how to pass variables between components

---
+++ src/components/Home.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1>Hello from Home component</h1>
+        <h1>{{data}}</h1>
+        <h1>{{message}}</h1>
+        <button v-on:click="hello" >Call Hello Function</button>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name:'Home',
+
+    props: {
+        data: String,
+        message: String
+    },
+    methods: {
+        hello(item) {
+            alert("hello function called")
+            console.log(item)
+        }
+
+    }
+}
+</script>
